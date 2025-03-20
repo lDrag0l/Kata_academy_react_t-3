a13bd46574dfa86bcd8b62f0bd3870bb571c81f6
ldrag0l <Drag0.des@mail.ru>
2025-03-20 21:51:10 +0300

    fix: path
    fix Card path

--- src/Components/CardList/CardList.jsx
+++ src/Components/CardList/CardList.jsx
@@ -6,7 +6,7 @@ import { selectAndSortTickets } from './../../redux/features/tickets/ticketsSele
 
 import { v4 as uuidv4 } from 'uuid';
 
-import Card from './../Card'
+import Card from './../Card/Card'
 import s from './CardList.module.scss'
 
 function CardList() {
